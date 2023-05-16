c3b70dada55cc59b465629ef9cf05fe8aa574516
Yufengx1 <yufengx1@student.unimelb.edu.au>
2023-05-17 00:08:41 +1000

    update map

--- front-end/src/Components/AppRoutes/index.js
+++ front-end/src/Components/AppRoutes/index.js
@@ -1,14 +1,14 @@
 import { BrowserRouter, Route, Routes } from "react-router-dom";
 import Customers from "../../Pages/Customers";
 import Dashboard from "../../Pages/Dashboard";
-import Inventory from "../../Pages/Inventory";
+import Map1 from "../../Pages/map1";
 import Orders from "../../Pages/Orders";
 
 function AppRoutes() {
   return (
     <Routes>
       <Route path="/" element={<Dashboard />}></Route>
-      <Route path="/inventory" element={<Inventory />}></Route>
+      <Route path="/map1" element={<Map1 />}></Route>
       <Route path="/orders" element={<Orders />}></Route>
       <Route path="/customers" element={<Customers />}></Route>
     </Routes>
