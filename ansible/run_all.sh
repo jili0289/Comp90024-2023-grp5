187c2663c15aa8201fa2dae8ebbaa19243795b77
Chang YU <chyu3@student.unimelb.edu.au>
2023-05-21 23:10:39 +1000

    manually merge Yu-Chang  and main brnach

---
+++ ansible/run_all.sh
@@ -0,0 +1,12 @@
+#!/bin/bash
+
+. unimelb-comp90024-2023-grp-5-openrc.sh
+
+# Configure basic instances/nodes
+ansible-playbook config_mrc.yaml -i ~/.ssh/yc20230516.pem
+
+# Deploy all instances/nodes environment
+ansible-playbook deploy_mrc.yaml -i inventory/hosts.ini
+
+# Lanuch mrc application
+# ansible-playbook launch_mrc.yaml -i inventory/hosts.ini
